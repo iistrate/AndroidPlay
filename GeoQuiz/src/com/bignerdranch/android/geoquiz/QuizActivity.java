dd7374dfdcaf69d6e7b6ab65aef31d6387c7dda4
Ioan George Istrate <istrate_ioan_george@yahoo.com>
2014-03-09 14:20:19 -0400

    Chapter 6

--- GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.geoquiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -17,6 +18,7 @@ public class QuizActivity extends Activity {
 	
 	private Button mTrueButton;
 	private Button mFalseButton;
+	private Button mCheatButton;
 	private ImageButton mNextButton;
 	private ImageButton mPreviousButton;
 	
@@ -29,7 +31,15 @@ public class QuizActivity extends Activity {
 			new TrueFalse(R.string.question_asia, true)
 	};
 	private int mCurrentIndex = 0;
+	private boolean mIsCheater;
 	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (data == null) {
+			return;
+		}
+		mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
+	}
 	
 	private void updateQuestion() {
 		int question = mQuestionBank[mCurrentIndex].getQuestion();
@@ -40,12 +50,15 @@ public class QuizActivity extends Activity {
 		
 		int messageResId = 0;
 		
-		if (userPressedTrue == answerIsTrue) {
-			messageResId = R.string.correct_toast;
-		} else {
-			messageResId = R.string.incorrect_toast;
+		if (mIsCheater) {
+			messageResId = R.string.judgement_toast;
+		} else {		
+			if (userPressedTrue == answerIsTrue) {
+				messageResId = R.string.correct_toast;
+			} else {
+				messageResId = R.string.incorrect_toast;
+			}
 		}
-		
 		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
 	}
 	
@@ -83,6 +96,7 @@ public class QuizActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				mCurrentIndex = (mCurrentIndex < 4) ? (mCurrentIndex + 1) : 0;
+				mIsCheater = false;
 				updateQuestion();
 			}
 		});
@@ -91,9 +105,21 @@ public class QuizActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				mCurrentIndex = (mCurrentIndex > 0) ? (mCurrentIndex - 1) : 4;
+				mIsCheater = false;
 				updateQuestion();
 			}
 		});
+		mCheatButton = (Button)findViewById(R.id.cheat_button);
+		mCheatButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+				boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+				i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+				startActivityForResult(i, 0);
+			}
+		});
+		
 		if (savedInstanceState != null) {
 			mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
 		}
