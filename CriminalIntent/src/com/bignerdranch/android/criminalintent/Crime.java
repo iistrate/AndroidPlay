919c271a2cc7b59ed38bf87232a6632ecaa7682a
Ioan George Istrate <istrate_ioan_george@yahoo.com>
2014-03-12 19:53:33 -0400

    ch 9

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/Crime.java
@@ -15,6 +15,10 @@ public class Crime {
 		mId = UUID.randomUUID();
 		mDate = new Date();
 	}
+	@Override
+	public String toString() {
+		return mTitle;
+	}
 
 	public String getTitle() {
 		return mTitle;
