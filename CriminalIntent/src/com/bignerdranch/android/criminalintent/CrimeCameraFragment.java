314e0f7ef33c9b6832e4da86aa6851b5c710ee89
Ioan George Istrate <istrate_ioan_george@yahoo.com>
2014-03-30 16:10:49 -0400

    chapter 21

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
@@ -1,9 +1,14 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.hardware.Camera;
 import android.hardware.Camera.Size;
 import android.os.Build;
@@ -20,8 +25,56 @@ import android.widget.Button;
 public class CrimeCameraFragment extends Fragment {
 	private static final String TAG = "CrimeCameraFragment";
 	
+	public static final String EXTRA_PHOTO_FILENAME = "com.bignerdranch.android.criminalintent.photo_filename";
+	
 	private Camera mCamera;
 	private SurfaceView mSurfaceView;
+	private View mProgressContainer;
+	
+	private Camera.ShutterCallback mShutterCallback = new Camera.ShutterCallback() {
+		@Override
+		public void onShutter() {
+			// Display the progress indicator
+			mProgressContainer.setVisibility(View.VISIBLE);
+		}
+	};
+	
+	private Camera.PictureCallback mJpegCallback = new Camera.PictureCallback() {
+		@Override
+		public void onPictureTaken(byte[] data, Camera camera) {
+			// create a filename
+			String filename = UUID.randomUUID().toString() + ".jpg";
+			//save the file to disk
+			FileOutputStream os = null;
+			boolean success = true;
+			try {
+				os = getActivity().openFileOutput(filename, Context.MODE_PRIVATE);
+				os.write(data);
+			} catch (Exception e) {
+				Log.e(TAG, "Error writing to file", e);
+				success = false;
+			} finally {
+				try {
+					if (os != null) {
+						os.close();
+					}
+				} 
+				catch (Exception e) {
+					Log.e(TAG, "Error closing file " + filename, e);
+					success = false;
+				}
+			}
+			// set the photo filename on the result intent
+			if (success) {
+				Intent i = new Intent();
+				i.putExtra(EXTRA_PHOTO_FILENAME, filename);
+				getActivity().setResult(Activity.RESULT_OK, i);
+			} else {
+				getActivity().setResult(Activity.RESULT_CANCELED);
+			}
+			getActivity().finish();
+		}
+	};
 	
 	@Override
 	@SuppressWarnings("deprecation")
@@ -33,8 +86,9 @@ public class CrimeCameraFragment extends Fragment {
 			
 			@Override
 			public void onClick(View v) {
-				getActivity().finish();
-				
+				if (mCamera != null) {
+					mCamera.takePicture(mShutterCallback, null, mJpegCallback);
+				}
 			}
 		});
 		
@@ -67,6 +121,8 @@ public class CrimeCameraFragment extends Fragment {
 				Camera.Parameters parameters = mCamera.getParameters();
 				Size s = getBestSupportedSize(parameters.getSupportedPreviewSizes(), w, h);
 				parameters.setPreviewSize(s.width, s.height);
+				s = getBestSupportedSize(parameters.getSupportedPictureSizes(), w, h);
+				parameters.setPictureSize(s.width, s.height);
 				mCamera.setParameters(parameters);
 				try {
 					mCamera.startPreview();
@@ -77,6 +133,9 @@ public class CrimeCameraFragment extends Fragment {
 				}
 			}
 		});
+		
+		mProgressContainer = v.findViewById(R.id.crime_camera_progressContainer);
+		mProgressContainer.setVisibility(View.INVISIBLE);
 		
 		return v;
 	}
--- CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -9,6 +9,7 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.BitmapDrawable;
 import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.NavUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,17 +29,22 @@ import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class CrimeFragment extends Fragment {
+	private static final String TAG = "CrimeFragment";
 	public static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
 	private static final String DIALOG_DATE = "date";
 	private static final int REQUEST_DATE = 0;
+	private static final int REQUEST_PHOTO = 1;
+
 	
 	private Crime mCrime;
 	private EditText mTitleField;
 	private Button mDateButton;
 	private CheckBox mSolvedCheckbox;
 	private ImageButton mPhotoButton;
+	private ImageView mPhotoView;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -100,8 +107,24 @@ public class CrimeFragment extends Fragment {
 			
 			@Override
 			public void onClick(View v) {
+				//launch camera activity
 				Intent i = new Intent(getActivity(), CrimeCameraActivity.class);
-				startActivity(i);
+				startActivityForResult(i, REQUEST_PHOTO);
+			}
+		});
+		
+		mPhotoView = (ImageView)v.findViewById(R.id.crime_imageView);
+		mPhotoView.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Photo p = mCrime.getPhoto();
+				if (p == null) {
+					return;
+				}
+				FragmentManager fm = getActivity().getSupportFragmentManager();
+				String path = getActivity().getFileStreamPath(p.getFilename()).getAbsolutePath();
+				ImageFragment.newInstance(path).show(fm, DIALOG_DATE);
 			}
 		});
 		
@@ -116,6 +139,19 @@ public class CrimeFragment extends Fragment {
 		
 		return v;
 	}
+	
+	private void showPhoto() {
+		// Reset the image button's image based on our photo
+		Photo p = mCrime.getPhoto();
+		BitmapDrawable b = null;
+		if (p != null) {
+			String path = getActivity()
+					.getFileStreamPath(p.getFilename()).getAbsolutePath();
+			b = PictureUtils.getScaledDrawable(getActivity(), path);
+		}
+		mPhotoView.setImageDrawable(b);
+	}
+	
 	public static CrimeFragment newInstance(UUID crimeID) {
 		Bundle args = new Bundle();
 		args.putSerializable(EXTRA_CRIME_ID, crimeID);
@@ -135,6 +171,17 @@ public class CrimeFragment extends Fragment {
 			Date date = (Date)data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
 			mCrime.setDate(date);
 			updateDate();
+		} else if (requestCode == REQUEST_PHOTO) {
+			//create a new photo object and attach it to the crime
+			String filename = data
+					.getStringExtra(CrimeCameraFragment.EXTRA_PHOTO_FILENAME);
+			if (filename != null) {
+				//Log.i(TAG, "filename + " + filename);
+				Photo p = new Photo(filename);
+				mCrime.setPhoto(p);
+				showPhoto();
+				//Log.i(TAG, "Crime: " + mCrime.getTitle() + " has photo");
+			}
 		}
 	}
 	private void updateDate() {
@@ -158,5 +205,15 @@ public class CrimeFragment extends Fragment {
 	public void onPause() {
 		super.onPause();
 		CrimeLab.get(getActivity()).saveCrimes();
+	}
+	@Override
+	public void onStart() {
+		super.onStart();
+		showPhoto();
+	}
+	@Override
+	public void onStop() {
+		super.onStop();
+		PictureUtils.cleanImageView(mPhotoView);
 	}
 }
